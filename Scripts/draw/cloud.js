c565457051135e15603b37a93898e2888df052a2
Nguyễn Thiện Nhân <nhanns23062012@gmail.com>
2026-04-17 00:41:40 +0700

    .

---
+++ Scripts/draw/cloud.js
@@ -0,0 +1,41 @@
+let clouds = [];
+const cloudImg = new Image();
+cloudImg.src = "../../Images/Clouds/cloud1.png"; 
+
+export function drawCloud(ctx, x, y, alpha = 0.8, scale = 1) {
+    ctx.save();
+
+    ctx.globalAlpha = alpha;
+    
+    ctx.drawImage(cloudImg, x, y, cloudImg.width * scale, cloudImg.height * scale)
+
+    ctx.restore();
+}
+
+export function addCloud(num) {
+    clouds = [];
+
+    for (let i = 0; i < num; i++) {
+        clouds.push({
+            x: Math.random() * window.innerWidth,
+            y: Math.random() * 200,
+            speed: 0.2 + Math.random() * 0.5,
+            alpha: 0.2 + Math.random() * 0.1,
+            scale: 0.2 + Math.random() * 0.2
+        });
+    }
+}
+
+export function updateClouds(ctx, canvas) {
+    clouds.forEach(c => {
+
+        c.x += c.speed;
+
+        if (c.x > canvas.width) {
+            c.x = -60;
+            c.y = Math.random() * 200;
+        }
+
+        drawCloud(ctx, c.x, c.y, c.alpha, c.scale);
+    });
+}
---
+++ Scripts/main.js
@@ -0,0 +1,23 @@
+import { addCloud, updateClouds } from './draw/cloud.js';
+
+const canvas = document.getElementById('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d');
+
+addCloud(8);
+
+function loop() {
+
+    ctx.fillStyle = '#0096eb';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    updateClouds(ctx, canvas);
+
+    requestAnimationFrame(loop);
+
+};
+
+loop();
