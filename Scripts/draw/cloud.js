2cad6765ec6a2ac8f8a9789aa6f48a8698ec7ea3
Nguyễn Thiện Nhân <nhanns23062012@gmail.com>
2026-04-17 01:12:23 +0700

    .

--- Scripts/draw/cloud.js
+++ Scripts/draw/cloud.js
@@ -2,6 +2,12 @@ let clouds = [];
 const cloudImg = new Image();
 cloudImg.src = new URL('../../Images/Clouds/cloud1.png', import.meta.url); 
 
+let loaded = false;
+
+cloudImg.onload = () => {
+    loaded = true;
+};
+
 export function drawCloud(ctx, x, y, alpha = 0.8, scale = 1) {
     ctx.save();
 
@@ -17,7 +23,7 @@ export function addCloud(num) {
 
     for (let i = 0; i < num; i++) {
         clouds.push({
-            x: Math.random() * window.innerWidth,
+            x: -100 - Math.random() * window.innerWidth,
             y: Math.random() * window.innerHeight,
             speed: 0.2 + Math.random() * 0.5,
             alpha: 0.4 + Math.random() * 0.1,
@@ -27,13 +33,16 @@ export function addCloud(num) {
 }
 
 export function updateClouds(ctx, canvas) {
+    if (!loaded) return;
+
     clouds.forEach(c => {
+        let CLOUD_WIDTH = cloudImg.width * c.scale;
 
         c.x += c.speed;
 
-        if (c.x > canvas.width) {
-            c.x = -60;
-            c.y = Math.random() * 200;
+        if (c.x > canvas.width + CLOUD_WIDTH) {
+            c.x = -CLOUD_WIDTH - Math.random() * window.innerWidth;
+            c.y = Math.random() * window.innerHeight;
         }
 
         drawCloud(ctx, c.x, c.y, c.alpha, c.scale);
