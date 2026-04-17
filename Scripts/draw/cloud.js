1ad9cc1e94e544eeadfa4994bcf45d904d075300
Nguyễn Thiện Nhân <nhanns23062012@gmail.com>
2026-04-17 23:14:29 +0700

    .

--- Scripts/draw/cloud.js
+++ Scripts/draw/cloud.js
@@ -1,70 +1,57 @@
+import { randomInt, randomFloat, randomList } from '../number/random.js';
 let clouds = [];
-
 let loaded = false;
 let firstLoad = true;
-
 const cloudImgs = [];
 let totalImages = 1;
 let loadedCount = 0;
-
 for (let i = 1; i <= totalImages; i++) {
     const img = new Image();
     img.src = new URL(`../../Images/Clouds/cloud${i}.png`, import.meta.url);
-
     img.onload = () => {
         loadedCount++;
         if (loadedCount === totalImages) {
             loaded = true;
         }
     };
-
     cloudImgs.push(img);
 }
-
 export function drawCloud(ctx, cloud) {
     ctx.save();
-
     ctx.globalAlpha = cloud.alpha;
-    
     ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.img.width * cloud.scale, cloud.img.height * cloud.scale );
-
     ctx.restore();
 }
-
 export function addCloud(num) {
     clouds = [];
-    let firstLoadNum = 5;
-
-    for (let i = 0; i < (firstLoad ? firstLoadNum : num); i++) {
+    for (let i = 0; i < (num); i++) {
         clouds.push({
-            img: cloudImgs[Math.floor(Math.random() * cloudImgs.length)],
-            x: firstLoad ? Math.random() * window.innerWidth : -100 - Math.random() * window.innerWidth,
-            y: Math.random() * window.innerHeight,
-            speed: 0.2 + Math.random() * 0.5,
-            dir: Math.random() > 0.5 ? 1 : -1,
-            alpha: 0.4 + Math.random() * 0.1,
-            scale: 0.15 + Math.random() * 0.2
+            img: randomList(cloudImgs),
+            x: randomInt(0, window.innerWidth),
+            y: randomInt(0, window.innerHeight),
+            speed: randomFloat(0.2, 0.8),
+            dir: randomList([1, -1]),
+            alpha: randomFloat(0.4, 0.5),
+            scale: randomFloat(0.15, 0.35)
         });
     }
-
     firstLoad = false;
 }
-
 export function updateClouds(ctx, canvas) {
     if (!loaded) return;
-
     clouds.forEach(c => {
         let CLOUD_WIDTH = c.img.width * c.scale;
-
+        let offsetMax = 20;
         c.x += c.speed * c.dir;
-
-        if (c.dir === 1 && c.x > canvas.width + CLOUD_WIDTH) {
-            c.x = -CLOUD_WIDTH - Math.random() * canvas.width;
-            c.y = Math.random() * canvas.height;
+        if (c.dir === 1 && c.x > canvas.width) {
+            c.x = -CLOUD_WIDTH - randomInt(0, offsetMax);
+            c.y = randomInt(0, canvas.height);
+            c.img = cloudImgs[randomInt(0, cloudImgs.length-1)];
         }
         if (c.dir === -1 && c.x < -CLOUD_WIDTH) {
-            c.x = canvas.width +  Math.random() * canvas.width;
-            c.y = Math.random() * canvas.height;
+            c.x = canvas.width + randomInt(0, offsetMax);
+            c.y = randomInt(0, canvas.height);
+            c.img = cloudImgs[randomInt(0, cloudImgs.length-1)];
         }
         drawCloud(ctx, c);
     });
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,24 +1,14 @@
 import { addCloud, updateClouds } from './draw/cloud.js';
 import { resizeCanvas } from './window/resize.js';
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
 resizeCanvas(canvas, ctx);
-
 addCloud(Math.floor(Math.random() * 6) + 9);
-
 function loop() {
-
     ctx.fillStyle = '#0096eb';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     updateClouds(ctx, canvas);
-
     requestAnimationFrame(loop);
-
 };
-
 loop();
-
 window.addEventListener("resize", resizeCanvas.bind(null, canvas, ctx));
