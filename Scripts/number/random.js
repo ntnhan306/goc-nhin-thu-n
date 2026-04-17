273bf266dca06986b1e0cca2f43dc515e3aed59e
Nguyễn Thiện Nhân <nhanns23062012@gmail.com>
2026-04-17 23:19:34 +0700

    .

--- Scripts/number/random.js
+++ Scripts/number/random.js
@@ -1,10 +1,3 @@
-export function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-export function randomFloat(min, max) {
-    return Math.random() * (max - min) + min;
-}
-export function randomList(list) {
-    if (!list || list.length === 0) return null;
-    return list[Math.floor(Math.random() * list.length)];
-}
+export function randomInt(min,max){return Math.floor(Math.random()*(max-min+1))+min}
+export function randomFloat(min,max){return Math.random()*(max-min)+min}
+export function randomList(list){return Array.isArray(list)&&list.length?list[Math.floor(Math.random()*list.length)]:null}
