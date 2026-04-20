b1d75af280e8fbd261881025bb891c9ade9ec780
Nguyễn Thiện Nhân <nhanns23062012@gmail.com>
2026-04-20 22:35:03 +0700

    .

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,9 +1,10 @@
 import { addCloud, updateClouds } from './draw/cloud.js';
 import { resizeCanvas } from './window/resize.js';
+import { randomInt } from './utils/random.js';
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 resizeCanvas(canvas, ctx);
-addCloud(Math.floor(Math.random() * 6) + 9);
+addCloud(randomInt(5, 15));
 function loop() {
     ctx.fillStyle = '#0096eb';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
